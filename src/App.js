248f4adedb98a85afc851934b309f87ea7ae5026
Swapnil Akulwar <91781600+swapya97@users.noreply.github.com>
2022-11-19 19:52:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <h1>The Evolution of Airbnb's Frontend</h1>
+   <p>@spikebrehm</p>
+   <img src="https://cached.imagescaler.hbpl.co.uk/resize/scaleWidth/743/cached.offlinehbpl.hbpl.co.uk/news/OMC/airbnbnewlogo-20140717072746371.jpg" alt="img"></img>
+   </div>
+  );
+}
+
+export default App;
